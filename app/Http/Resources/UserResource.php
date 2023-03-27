73220826220b5f8e9fd3bb16fd7523f0cf3fc8de
AlexRogaleski <rogaleski@gmail.com>
2023-03-27 16:50:33 -0300

    formata retorno de UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,14 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'is_admin' => $this->active == 1 ? true : false,
+            'created_at' => $this->created_at,
+            'token' => $this->token,
+        ];
+        // return parent::toArray($request);
     }
 }
